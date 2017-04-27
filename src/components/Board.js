57152bcb0ff84a817cc2eba2c7afc3543484b8c1
Stephanie Zeng <stephaniezeng521@gmail.com>
2017-04-26 20:36:55 -0400

    Initial commit

---
+++ src/components/Board.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import Row from './Row';
+
+class Board extends Component {
+  render() {
+    return (
+      <Row />
+    );
+  }
+}
+
+export default Board;
---
+++ src/components/Row.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import Cell from './Cell';
+
+class Row extends Component {
+  render() {
+    return (
+      <Cell />
+    );
+  }
+}
+
+export default Row;
